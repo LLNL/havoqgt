5fa0fad9397a4b10078072d642697f5ee6239c9f
Keita Iwabuchi <iwabuchi.k.ab@m.titech.ac.jp>
2015-12-01 10:32:25 -0800

    changed boost 1.56 to 1.59

--- scripts/catalyst.llnl.gov/do_cmake_debug.sh
+++ scripts/catalyst.llnl.gov/do_cmake_debug.sh
@@ -14,6 +14,6 @@ cmake ../../ \
   -DCMAKE_BUILD_TYPE=Debug \
   -DMPI_C_COMPILER=/usr/local/tools/mvapich2-gnu-1.9/bin/mpicc \
   -DMPI_CXX_COMPILER=/usr/local/tools/mvapich2-gnu-1.9/bin/mpicxx \
-  -DBOOST_ROOT=/usr/gapps/dst/opt/boost_stages/boost_1_56_0/ \
+  -DBOOST_ROOT=$HOME/apps/boost_1_59_0/ \
   -DCMAKE_CXX_FLAGS="-std=c++11 -O0 -g3 -lrt " \
   -DCMAKE_C_FLAGS="-std=c++11 -O0 -g3  -lrt " \
