4cd6caa98a4981a119d26eb3e69517a69bea0349
Tahsin Arafat Reza <reza2@llnl.gov>
2017-03-07 13:48:35 -0800

    adding edge state update feature

---
+++ include/havoqgt/update_edge_state.hpp
@@ -0,0 +1,18 @@
+#ifndef HAVOQGT_UPDATE_EDGE_STATE_HPP_INCLUDED
+#define HAVOQGT_UPDATE_EDGE_STATE_HPP_INCLUDED
+
+#include <havoqgt/visitor_queue.hpp>
+#include <havoqgt/detail/visitor_priority_queue.hpp>
+
+namespace havoqgt { namespace mpi {
+
+void update_edge_state() {
+  std::cout << "update_edge_state.hpp" << std::endl;
+} 
+
+}} //end namespace havoqgt::mpi
+
+#endif //HAVOQGT_UPDATE_EDGE_STATE_HPP_INCLUDED 
+
+
+
