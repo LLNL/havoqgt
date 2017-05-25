0dbf65b756262bd87be2ee70ef76d82c762e6b83
Roger Pearce <rpearce@{gmail.com,tamu.edu,llnl.gov}>
2017-05-25 09:30:28 -0700

    Updated boost paths & mpi

--- scripts/catalyst.llnl.gov/do_cmake.sh
+++ scripts/catalyst.llnl.gov/do_cmake.sh
@@ -10,8 +10,8 @@ cmake ../../ \
   -DHAVOQGT_BUILD_TEST=TRUE \
   -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
   -DCMAKE_BUILD_TYPE=Release \
-  -DMPI_C_COMPILER=/usr/tce/packages/openmpi/openmpi-2.0.0-gcc-4.9.3/bin/mpicc \
-  -DMPI_CXX_COMPILER=/usr/tce/packages/openmpi/openmpi-2.0.0-gcc-4.9.3/bin/mpicxx \
-  -DBOOST_ROOT=/g/g90/reza2/boost_1_57_0/ \
+  -DMPI_C_COMPILER=/usr/tce/packages/mvapich2/mvapich2-2.2-gcc-4.9.3/bin/mpicc \
+  -DMPI_CXX_COMPILER=/usr/tce/packages/mvapich2/mvapich2-2.2-gcc-4.9.3/bin/mpicxx \
+  -DBOOST_ROOT=/usr/tce/packages/boost/boost-1.58.0-mvapich2-2.2-intel-16.0.3 \
   -DCMAKE_CXX_FLAGS="-std=c++11 -lrt -lpthread -lm" \
 
