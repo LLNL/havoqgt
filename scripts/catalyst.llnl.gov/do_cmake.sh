6946f8011667938fd08f8048a092b064627f16c1
Roger Pearce <rpearce@{gmail.com,tamu.edu,llnl.gov}>
2017-06-27 19:02:33 -0700

    added numproc flag for test

--- scripts/catalyst.llnl.gov/do_cmake.sh
+++ scripts/catalyst.llnl.gov/do_cmake.sh
@@ -13,5 +13,6 @@ cmake ../../ \
   -DMPI_C_COMPILER=/usr/tce/packages/mvapich2/mvapich2-2.2-gcc-4.9.3/bin/mpicc \
   -DMPI_CXX_COMPILER=/usr/tce/packages/mvapich2/mvapich2-2.2-gcc-4.9.3/bin/mpicxx \
   -DBOOST_ROOT=/usr/tce/packages/boost/boost-1.58.0-mvapich2-2.2-intel-16.0.3 \
+  -DMPIEXEC_NUMPROC_FLAG="-n" \
   -DCMAKE_CXX_FLAGS="-std=c++11 -lrt -lpthread -lm" \
 
