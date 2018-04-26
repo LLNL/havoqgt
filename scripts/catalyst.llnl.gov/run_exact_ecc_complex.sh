9883bd9deab2e06b31f87cf80c1d39055911efcd
Keita Iwabuchi <iwabuchi1@llnl.gov>
2018-04-26 13:22:05 -0700

    Updates in script

--- scripts/catalyst.llnl.gov/run_exact_ecc_complex.sh
+++ scripts/catalyst.llnl.gov/run_exact_ecc_complex.sh
@@ -53,12 +53,11 @@ unset USE_NEW_ADP
 if [ "${tag}" == "tk" ]; then
     option="export USE_TAKE=1" ## 1 is a dummy value
     source_selection_algorithms=""
-elif [ "${tag}" == "adp_tk_pr" ]; then
-    option="export USE_TAKE_PRUN=1" ## 1 is a dummy value
-    source_selection_algorithms="-a 0:1:2:3:4:5:6:7"
-elif [ "${tag}" == "adp" ]; then
-    source_selection_algorithms="-a 0:1:2:3:4:5:6:7"
 elif [ "${tag}" == "adp_new" ]; then
+    option="export USE_NEW_ADP=10"
+    source_selection_algorithms="-a 0:1:2:3:4:5:6:7"
+elif [ "${tag}" == "adp_new_tk_pr" ]; then
+    option="export USE_TAKE_PRUN=1" ## 1 is a dummy value
     option="export USE_NEW_ADP=10"
     source_selection_algorithms="-a 0:1:2:3:4:5:6:7"
 fi
