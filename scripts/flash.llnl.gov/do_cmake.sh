0c38189b5e1feb22460a41265d05579a03dc695e
Roger Pearce <rpearce@llnl.gov>
2018-05-26 16:26:59 -0700

    Updating MPI path

--- scripts/flash.llnl.gov/do_cmake.sh
+++ scripts/flash.llnl.gov/do_cmake.sh
@@ -10,7 +10,7 @@ cmake ../../ \
   -DHAVOQGT_BUILD_TEST=TRUE \
   -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
   -DCMAKE_BUILD_TYPE=Release \
-  -DMPI_C_COMPILER=/usr/tce/packages/openmpi/openmpi-2.0.0-gcc-4.9.3/bin/mpicc \
-  -DMPI_CXX_COMPILER=/usr/tce/packages/openmpi/openmpi-2.0.0-gcc-4.9.3/bin/mpicxx \
+  -DMPI_C_COMPILER=/usr/tce/packages/openmpi/openmpi-3.0.1-gcc-4.9.3/bin/mpicc \
+  -DMPI_CXX_COMPILER=/usr/tce/packages/openmpi/openmpi-3.0.1-gcc-4.9.3/bin/mpic++ \
   -DBOOST_ROOT=/usr/tce/packages/boost/boost-1.58.0-mvapich2-2.2-intel-16.0.3 \
   -DCMAKE_CXX_FLAGS="-std=c++11 -lrt -lpthread -lm" \
