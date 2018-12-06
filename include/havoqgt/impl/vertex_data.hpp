9cb6969891a918eaa75ca5e95e94d10c21c8ed49
tahsinreza <reza2@llnl.gov>
2018-12-06 15:21:30 -0800

    PruneJuice

--- include/havoqgt/impl/vertex_data.hpp
+++ include/havoqgt/impl/vertex_data.hpp
@@ -94,10 +94,33 @@ class delegate_partitioned_graph<SegementManager>::vertex_data {
     }
   }
 
+  void clear() {
+    for(size_t i=0; i<m_owned_vert_data.size(); ++i) {
+      m_owned_vert_data[i].clear();
+    }
+    for(size_t i=0; i<m_delegate_data.size(); ++i) {
+      m_delegate_data[i].clear();
+    }
+  }
+
   void all_reduce() {
     std::vector<T> tmp_in(m_delegate_data.begin(), m_delegate_data.end());
     std::vector<T> tmp_out(tmp_in.size(), 0);
     mpi_all_reduce(tmp_in, tmp_out, std::plus<T>(), MPI_COMM_WORLD);
+    std::copy(tmp_out.begin(), tmp_out.end(), m_delegate_data.begin());
+  }
+
+  void all_max_reduce() {
+    std::vector<T> tmp_in(m_delegate_data.begin(), m_delegate_data.end());
+    std::vector<T> tmp_out(tmp_in.size(), 0);
+    mpi_all_reduce(tmp_in, tmp_out, std::greater<T>(), MPI_COMM_WORLD);
+    std::copy(tmp_out.begin(), tmp_out.end(), m_delegate_data.begin());
+  }
+
+  void all_min_reduce() {
+    std::vector<T> tmp_in(m_delegate_data.begin(), m_delegate_data.end());
+    std::vector<T> tmp_out(tmp_in.size(), 0);
+    mpi_all_reduce(tmp_in, tmp_out, std::less<T>(), MPI_COMM_WORLD);
     std::copy(tmp_out.begin(), tmp_out.end(), m_delegate_data.begin());
   }
     
