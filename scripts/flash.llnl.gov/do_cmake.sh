768e771a4303590a3fbd115e162ff98ae2f887c4
tahsinreza <tahsin.reza.dev@gmail.com>
2018-12-09 19:10:26 -0800

    updating do_cmake.sh script for flash.llnl.gov

--- scripts/flash.llnl.gov/do_cmake.sh
+++ scripts/flash.llnl.gov/do_cmake.sh
@@ -10,7 +10,10 @@ cmake ../../ \
   -DHAVOQGT_BUILD_TEST=TRUE \
   -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
   -DCMAKE_BUILD_TYPE=Release \
-  -DMPI_C_COMPILER=/usr/tce/packages/openmpi/openmpi-3.0.1-gcc-4.9.3/bin/mpicc \
-  -DMPI_CXX_COMPILER=/usr/tce/packages/openmpi/openmpi-3.0.1-gcc-4.9.3/bin/mpic++ \
-  -DBOOST_ROOT=/usr/tce/packages/boost/boost-1.58.0-mvapich2-2.2-intel-16.0.3 \
-  -DCMAKE_CXX_FLAGS="-std=c++11 -lrt -lpthread -lm" \
+  -DCMAKE_C_COMPILER=/usr/tce/packages/gcc/gcc-8.1.0/bin/gcc \
+  -DCMAKE_CXX_COMPILER=/usr/tce/packages/gcc/gcc-8.1.0/bin/g++ \
+  -DMPI_C_COMPILER=/usr/tce/packages/mvapich2/mvapich2-2.2-gcc-8.1.0/bin/mpicc \
+  -DMPI_CXX_COMPILER=/usr/tce/packages/mvapich2/mvapich2-2.2-gcc-8.1.0/bin/mpicxx \
+  -DBOOST_ROOT=/usr/tce/packages/boost/boost-1.66.0-mvapich2-2.2-gcc-4.9.3 \
+  -DMPIEXEC_NUMPROC_FLAG="-n" \
+  -DCMAKE_CXX_FLAGS="-std=c++14 -lrt -lstdc++fs -lpthread" \
