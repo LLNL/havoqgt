3db0d255f5c99bdb47ae5431073d45ae9456ce07
Trevor Steil <steil1@llnl.gov>
2018-12-11 19:01:59 -0800

    Moved kronecker generator to include/havoqgt

--- include/havoqgt/kronecker_edge_generator.hpp
+++ include/havoqgt/kronecker_edge_generator.hpp
@@ -1,3 +1,57 @@
+/*
+ * Copyright (c) 2013, Lawrence Livermore National Security, LLC.
+ * Produced at the Lawrence Livermore National Laboratory.
+ * Written by Roger Pearce <rpearce@llnl.gov>.
+ * LLNL-CODE-644630.
+ * All rights reserved.
+ *
+ * This file is part of HavoqGT, Version 0.1.
+ * For details, see
+ * https://computation.llnl.gov/casc/dcca-pub/dcca/Downloads.html
+ *
+ * Please also read this link – Our Notice and GNU Lesser General Public
+ * License. http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License (as published by the Free
+ * Software Foundation) version 2.1 dated February 1999.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the IMPLIED WARRANTY OF MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the terms and conditions of the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * OUR NOTICE AND TERMS AND CONDITIONS OF THE GNU GENERAL PUBLIC LICENSE
+ *
+ * Our Preamble Notice
+ *
+ * A. This notice is required to be provided under our contract with the
+ * U.S. Department of Energy (DOE). This work was produced at the Lawrence
+ * Livermore National Laboratory under Contract No. DE-AC52-07NA27344 with the
+ * DOE.
+ *
+ * B. Neither the United States Government nor Lawrence Livermore National
+ * Security, LLC nor any of their employees, makes any warranty, express or
+ * implied, or assumes any liability or responsibility for the accuracy,
+ * completeness, or usefulness of any information, apparatus, product, or
+ * process disclosed, or represents that its use would not infringe
+ * privately-owned rights.
+ *
+ * C. Also, reference herein to any specific commercial products, process, or
+ * services by trade name, trademark, manufacturer or otherwise does not
+ * necessarily constitute or imply its endorsement, recommendation, or favoring
+ * by the United States Government or Lawrence Livermore National Security, LLC.
+ * The views and opinions of authors expressed herein do not necessarily state
+ * or reflect those of the United States Government or Lawrence Livermore
+ * National Security, LLC, and shall not be used for advertising or product
+ * endorsement purposes.
+ *
+ */
+
 #pragma once
 
 #include <assert.h>
@@ -13,6 +67,8 @@
 #include <tuple>
 #include <vector>
 #include <ygm/mpi.hpp>
+
+namespace kronecker {
 
 template <typename T, typename W>
 T read_graph_file(std::string                       filename,
@@ -52,6 +108,10 @@ T read_graph_file(std::string                       filename,
 
   return num_vertices;
 }
+
+}  // end namespace kronecker
+
+namespace havoqgt {
 
 template <typename edge_data_type = uint8_t, typename Small_Index = uint32_t,
           typename C1 =
@@ -166,8 +226,8 @@ class kronecker_edge_generator {
         m_undirected(undirected),
         m_graph1_pos(ygm::comm_world().rank()),
         m_has_edge_data(true) {
-    m_num_vertices_graph1 = read_graph_file(filename1, m_graph1);
-    m_num_vertices_graph2 = read_graph_file(filename2, m_graph2);
+    m_num_vertices_graph1 = kronecker::read_graph_file(filename1, m_graph1);
+    m_num_vertices_graph2 = kronecker::read_graph_file(filename2, m_graph2);
     m_local_edge_count =
         m_graph1.size() * m_graph2.size() / ygm::comm_world().size() +
         (ygm::comm_world().rank() <
@@ -252,3 +312,5 @@ class kronecker_edge_generator {
   typename C1::iterator m_graph1_itr;
   typename C2::iterator m_graph2_itr;
 };
+
+}  // end namespace havoqgt
