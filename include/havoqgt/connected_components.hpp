095723b10b53420d16fa9f6dae7667feb33b3e6f
Roger Pearce <rpearce@llnl.gov>
2020-10-15 09:42:10 -0700

    Adding predicates to CC

--- include/havoqgt/connected_components.hpp
+++ include/havoqgt/connected_components.hpp
@@ -6,53 +6,56 @@
 #ifndef HAVOQGT_CONNECTED_COMPONENTS_HPP_INCLUDED
 #define HAVOQGT_CONNECTED_COMPONENTS_HPP_INCLUDED
 
-
-#include <havoqgt/visitor_queue.hpp>
 #include <havoqgt/detail/visitor_priority_queue.hpp>
+#include <havoqgt/visitor_queue.hpp>
 
 namespace havoqgt {
 
-template<typename Graph>
+template <typename Graph>
 class cc_visitor {
  public:
-  typedef typename Graph::vertex_locator                 vertex_locator;
-  cc_visitor() { }
+  typedef typename Graph::vertex_locator vertex_locator;
+  cc_visitor() {}
   cc_visitor(vertex_locator _vertex, vertex_locator _cc)
-      : vertex(_vertex)
-      , m_cc(_cc) { }
+      : vertex(_vertex), m_cc(_cc) {}
 
-  cc_visitor(vertex_locator _vertex)
-      : vertex(_vertex)
-      , m_cc(_vertex) { }
+  cc_visitor(vertex_locator _vertex) : vertex(_vertex), m_cc(_vertex) {}
 
-  template<typename AlgData>
+  template <typename AlgData>
   bool pre_visit(AlgData& alg_data) const {
-    auto& graph = std::get<0>(alg_data);
+    auto& graph   = std::get<0>(alg_data);
     auto& cc_data = std::get<1>(alg_data);
-    if(graph.locator_to_label(m_cc) < graph.locator_to_label(cc_data[vertex])) {
+    if (graph.locator_to_label(m_cc) <
+        graph.locator_to_label(cc_data[vertex])) {
       cc_data[vertex] = m_cc;
       return true;
     }
     return false;
   }
 
-  template<typename VisitorQueueHandle, typename AlgData>
-  bool init_visit(Graph& g, VisitorQueueHandle vis_queue, AlgData& alg_data) const {
+  template <typename VisitorQueueHandle, typename AlgData>
+  bool init_visit(Graph& g, VisitorQueueHandle vis_queue,
+                  AlgData& alg_data) const {
     return visit(g, vis_queue, alg_data);
   }
 
-  template<typename VisitorQueueHandle, typename AlgData>
+  template <typename VisitorQueueHandle, typename AlgData>
   bool visit(Graph& g, VisitorQueueHandle vis_queue, AlgData& alg_data) const {
-    auto& graph = std::get<0>(alg_data);
-    auto& cc_data = std::get<1>(alg_data);
+    auto& graph     = std::get<0>(alg_data);
+    auto& cc_data   = std::get<1>(alg_data);
+    auto& predicate = std::get<2>(alg_data);
 
-    if(graph.locator_to_label(cc_data[vertex]) >= graph.locator_to_label(m_cc)) {
+    if (graph.locator_to_label(cc_data[vertex]) >=
+        graph.locator_to_label(m_cc)) {
       cc_data[vertex] = m_cc;
-      for(auto eitr = g.edges_begin(vertex); eitr != g.edges_end(vertex); ++eitr) {
+      for (auto eitr = g.edges_begin(vertex); eitr != g.edges_end(vertex);
+           ++eitr) {
         auto neighbor = eitr.target();
-        if(graph.locator_to_label(m_cc) < graph.locator_to_label(neighbor)) {
-          cc_visitor new_visitor(neighbor, m_cc);
-          vis_queue->queue_visitor(new_visitor);
+        if (predicate(vertex, neighbor, eitr)) {
+          if (graph.locator_to_label(m_cc) < graph.locator_to_label(neighbor)) {
+            cc_visitor new_visitor(neighbor, m_cc);
+            vis_queue->queue_visitor(new_visitor);
+          }
         }
       }
       return true;
@@ -60,45 +63,45 @@ class cc_visitor {
     return false;
   }
 
-
   friend inline bool operator>(const cc_visitor& v1, const cc_visitor& v2) {
-    if(v2.m_cc < v1.m_cc)
-    {
+    if (v2.m_cc < v1.m_cc) {
       return true;
-    } else if(v1.m_cc < v2.m_cc)
-    {
+    } else if (v1.m_cc < v2.m_cc) {
       return false;
     }
-    if(v1.vertex == v2.vertex) return false;
+    if (v1.vertex == v2.vertex) return false;
     return !(v1.vertex < v2.vertex);
   }
 
-  vertex_locator   vertex;
-  vertex_locator  m_cc;
+  vertex_locator vertex;
+  vertex_locator m_cc;
 };
 
 template <typename TGraph, typename CCData>
 void connected_components(TGraph* g, CCData& cc_data) {
+  auto predicate = [](const auto& vi, const auto& vj, const auto& ei) {
+    return true;
+  };
+  connected_components(g, cc_data, predicate);
+}
 
-  typedef  cc_visitor<TGraph>    visitor_type;
+template <typename TGraph, typename CCData, typename Function>
+void connected_components(TGraph* g, CCData& cc_data, Function& predicate) {
+  typedef cc_visitor<TGraph> visitor_type;
 
-  for(auto vitr = g->vertices_begin(); vitr != g->vertices_end(); ++vitr) {
+  for (auto vitr = g->vertices_begin(); vitr != g->vertices_end(); ++vitr) {
     cc_data[*vitr] = *vitr;
   }
-  for(auto citr = g->controller_begin(); citr != g->controller_end(); ++citr) {
+  for (auto citr = g->controller_begin(); citr != g->controller_end(); ++citr) {
     cc_data[*citr] = *citr;
   }
 
-  auto alg_data = std::forward_as_tuple(*g, cc_data);
-  auto vq = create_visitor_queue<visitor_type, detail::visitor_priority_queue>(g, alg_data);
+  auto alg_data = std::forward_as_tuple(*g, cc_data, predicate);
+  auto vq = create_visitor_queue<visitor_type, detail::visitor_priority_queue>(
+      g, alg_data);
   vq.init_visitor_traversal();
 }
 
+}  // end namespace havoqgt
 
-
-} //end namespace havoqgt
-
-
-
-
-#endif //HAVOQGT_CONNECTED_COMPONENTS_HPP_INCLUDED
+#endif  // HAVOQGT_CONNECTED_COMPONENTS_HPP_INCLUDED
--- src/run_cc.cpp
+++ src/run_cc.cpp
@@ -25,16 +25,20 @@ using namespace havoqgt;
 
 void usage() {
   if (comm_world().rank() == 0) {
-    std::cerr << "Usage: -i <string> -s <int>\n"
-              << " -i <string>   - input graph base filename (required)\n"
-              << " -b <string>   - backup graph base filename.  If set, "
-                 "\"input\" graph will be deleted if it exists\n"
-              << " -h            - print help and exit\n\n";
+    std::cerr
+        << "Usage: -i <string> -s <int>\n"
+        << " -i <string>   - input graph base filename (required)\n"
+        << " -b <string>   - backup graph base filename.  If set, "
+           "\"input\" graph will be deleted if it exists\n"
+        << " -w <double>   - Filter edge weights greater than weight value \n"
+        << " -h            - print help and exit\n\n";
   }
 }
 
 void parse_cmd_line(int argc, char** argv, std::string& input_filename,
-                    std::string& backup_filename) {
+                    std::string& backup_filename, bool& has_edge_filter,
+                    double& edge_filter, std::string& output_base_filename,
+                    size_t& print_threshold) {
   if (comm_world().rank() == 0) {
     std::cout << "CMD line:";
     for (int i = 0; i < argc; ++i) {
@@ -44,10 +48,12 @@ void parse_cmd_line(int argc, char** argv, std::string& input_filename,
   }
 
   bool found_input_filename = false;
+  has_edge_filter           = false;
+  print_threshold           = 0;
 
   char c;
   bool prn_help = false;
-  while ((c = getopt(argc, argv, "i:b:h ")) != -1) {
+  while ((c = getopt(argc, argv, "i:b:w:o:p:h ")) != -1) {
     switch (c) {
       case 'h':
         prn_help = true;
@@ -58,6 +64,16 @@ void parse_cmd_line(int argc, char** argv, std::string& input_filename,
         break;
       case 'b':
         backup_filename = optarg;
+        break;
+      case 'w':
+        has_edge_filter = true;
+        edge_filter     = atof(optarg);
+        break;
+      case 'o':
+        output_base_filename = optarg;
+        break;
+      case 'p':
+        print_threshold = atoi(optarg);
         break;
       default:
         std::cerr << "Unrecognized option: " << c << ", ignore." << std::endl;
@@ -73,6 +89,8 @@ void parse_cmd_line(int argc, char** argv, std::string& input_filename,
 
 int main(int argc, char** argv) {
   typedef delegate_partitioned_graph<distributed_db::allocator<>> graph_type;
+  typedef double                                    edge_data_type;
+  typedef distributed_db::allocator<edge_data_type> edge_data_allocator_type;
 
   int mpi_rank(0), mpi_size(0);
 
@@ -90,8 +108,13 @@ int main(int argc, char** argv) {
 
     std::string graph_input;
     std::string backup_filename;
+    std::string output_base_filename;
+    bool        has_edge_filter;
+    double      edge_filter;
+    size_t      print_threshold;
 
-    parse_cmd_line(argc, argv, graph_input, backup_filename);
+    parse_cmd_line(argc, argv, graph_input, backup_filename, has_edge_filter,
+                   edge_filter, output_base_filename, print_threshold);
 
     MPI_Barrier(MPI_COMM_WORLD);
     if (backup_filename.size() > 0) {
@@ -116,50 +139,82 @@ int main(int argc, char** argv) {
 
     MPI_Barrier(MPI_COMM_WORLD);
     double time_start = MPI_Wtime();
-    connected_components(graph, cc_data);
+    if (!has_edge_filter) {
+      connected_components(graph, cc_data);
+    } else {
+      auto edge_weights =
+          ddb.get_manager()
+              ->find<graph_type::edge_data<edge_data_type,
+                                           edge_data_allocator_type>>(
+                  "graph_edge_data_obj")
+              .first;
+    }
     MPI_Barrier(MPI_COMM_WORLD);
     double time_end = MPI_Wtime();
 
-    std::map<uint64_t, uint64_t> cc_count;
-    for (auto vitr = graph->vertices_begin(); vitr != graph->vertices_end();
-         ++vitr) {
-      if (graph->degree(*vitr) > 0) {
-        cc_count[graph->locator_to_label(cc_data[*vitr])]++;
+    if (print_threshold > 0) {
+      std::map<uint64_t, uint64_t> cc_count;
+      for (auto vitr = graph->vertices_begin(); vitr != graph->vertices_end();
+           ++vitr) {
+        if (graph->degree(*vitr) > 0) {
+          cc_count[graph->locator_to_label(cc_data[*vitr])]++;
+        }
       }
-    }
 
-    for (auto citr = g->controller_begin(); citr != g->controller_end();
-         ++citr) {
-      if (graph->degree(*vitr) > 0) {
-        cc_count[graph->locator_to_label(cc_data[*vitr])]++;
+      for (auto citr = graph->controller_begin();
+           citr != graph->controller_end(); ++citr) {
+        if (graph->degree(*citr) > 0) {
+          cc_count[graph->locator_to_label(cc_data[*citr])]++;
+        }
       }
-    }
 
-    auto     cc_count_itr = cc_count.begin();
-    uint64_t largest_cc   = 0;
-    uint64_t num_ccs      = 0;
-    while (!detail::global_iterator_range_empty(cc_count_itr, cc_count.end(),
-                                                MPI_COMM_WORLD)) {
-      uint64_t local_next_cc = cc_count_itr->first;
-      uint64_t global_next_cc =
-          mpi_all_reduce(local_next_cc, std::less<uint64_t>(), MPI_COMM_WORLD);
-      uint64_t local_count =
-          (local_next_cc == global_next_cc) ? (cc_count_itr++)->second : 0;
-      uint64_t global_cc_count =
-          mpi_all_reduce(local_count, std::plus<uint64_t>(), MPI_COMM_WORLD);
+      auto     cc_count_itr = cc_count.begin();
+      uint64_t largest_cc   = 0;
+      uint64_t num_ccs      = 0;
+      while (!detail::global_iterator_range_empty(cc_count_itr, cc_count.end(),
+                                                  MPI_COMM_WORLD)) {
+        uint64_t local_next_cc  = cc_count_itr->first;
+        uint64_t global_next_cc = mpi_all_reduce(
+            local_next_cc, std::less<uint64_t>(), MPI_COMM_WORLD);
+        uint64_t local_count =
+            (local_next_cc == global_next_cc) ? (cc_count_itr++)->second : 0;
+        uint64_t global_cc_count =
+            mpi_all_reduce(local_count, std::plus<uint64_t>(), MPI_COMM_WORLD);
+        if (mpi_rank == 0) {
+          std::cout << "CC " << global_next_cc << ", size = " << global_cc_count
+                    << std::endl;
+        }
+        largest_cc = std::max(global_cc_count, largest_cc);
+        num_ccs++;
+      }
       if (mpi_rank == 0) {
-        std::cout << "CC " << global_next_cc << ", size = " << global_cc_count
+        std::cout << "Num CCs = " << num_ccs << ", largest CC = " << largest_cc
+                  << ", Traversal Time = " << time_end - time_start
                   << std::endl;
       }
-      largest_cc = std::max(global_cc_count, largest_cc);
-      num_ccs++;
     }
-    if (mpi_rank == 0) {
-      std::cout << "Num CCs = " << num_ccs << ", largest CC = " << largest_cc
-                << ", Traversal Time = " << time_end - time_start << std::endl;
-    }
-  }  // END Main MPI
-  ;
+    if (output_base_filename.size() > 0) {
+      std::stringstream output_filename;
+      output_filename << output_base_filename << "_" << mpi_rank;
+      std::ofstream ofs(output_filename.str().c_str());
 
+      for (auto vitr = graph->vertices_begin(); vitr != graph->vertices_end();
+           ++vitr) {
+        if (graph->degree(*vitr) > 0) {
+          ofs << graph->locator_to_label(*vitr) << " "
+              << graph->locator_to_label(cc_data[*vitr]) << "\n";
+        }
+      }
+
+      for (auto citr = graph->controller_begin();
+           citr != graph->controller_end(); ++citr) {
+        if (graph->degree(*citr) > 0) {
+          ofs << graph->locator_to_label(*citr) << " "
+              << graph->locator_to_label(cc_data[*citr]) << "\n";
+        }
+      }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+  }  // END Main MPI
   return 0;
 }
